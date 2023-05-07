1ae298682acbd4502196caa9c42a5c382f0da2b4
Andrey Tzarkov <admyndc@gmail.com>
2023-05-07 15:03:39 +0300

    Screen Record

--- ScreenRec.MainWindow/MainWindow.xaml.cs
+++ ScreenRec.MainWindow/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using ScreenRec.Settings;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ScreenRec.ScreeanArea;
 using System.ComponentModel;
-using System.CodeDom;
-using System.Threading;
 
 namespace ScreenRec.MainWindow
 {
--- ScreenRec.Settings/Settings.cs
+++ ScreenRec.Settings/Settings.cs
@@ -1,5 +1,4 @@
-﻿using System.Drawing;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace ScreenRec.Settings
 {
